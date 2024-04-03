ce7ca9b33b6b86add8d9ac33832b8607a6f86e97
faisalraouf <faisal@tiecodes.com>
2024-04-04 02:13:20 +0500

    6th  excercise completed

---
+++ exercise6.py
@@ -0,0 +1,10 @@
+# Define a list of numbers
+numbers_list = [10, 15, 20, 25, 30]
+
+# Iterate through the list
+for number in numbers_list:
+    # Check if the number is divisible by 5
+    if number % 5 == 0:
+        # Print the number if divisible by 5
+        print(number)
+    
