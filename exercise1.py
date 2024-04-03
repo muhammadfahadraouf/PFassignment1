668ebd0c22d60507f698835bfc2eb8f2489190d9
faisalraouf <faisal@tiecodes.com>
2024-04-04 01:38:14 +0500

    First excercise completed

---
+++ exercise1.py
@@ -0,0 +1,13 @@
+# Taking input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+# Calculating product and sum
+product = num1 * num2
+summation = num1 + num2
+
+# Checking the condition and printing the result
+if product <= 1000:
+    print("The product is:", product)
+else:
+    print("The sum is:", summation)
