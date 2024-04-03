bba0353a5448ed9668c9ef9bda90536cd740d662
faisalraouf <faisal@tiecodes.com>
2024-04-04 01:58:26 +0500

    5th  excercise completed

---
+++ exercise5.py
@@ -0,0 +1,8 @@
+# Define a list of numbers
+numbers_list = [5, 10, 15, 20, 25]
+
+# Check if the first and last numbers are the same
+if numbers_list[0] == numbers_list[-1]:
+    print("First and Last Numbers are same")
+else:
+    print("Not Same")
